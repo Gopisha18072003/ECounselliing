c3fe67f5134e124e69da2a62396fe699aac96388
Gopisha18072003 <gopishaw111@gmail.com>
2025-05-11 18:43:03 +0530

    fixing counselling tab 2

--- src/pages/student/dashboard/Counselling.jsx
+++ src/pages/student/dashboard/Counselling.jsx
@@ -64,7 +64,7 @@ export default function Counselling() {
       <div className="flex-1 overflow-auto">
         {/* Loading Spinner */}
         {(isStatusPending || isApplicationPending) && (
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center h-full">
             <CircularProgress color="primary" />
           </div>
         )}
